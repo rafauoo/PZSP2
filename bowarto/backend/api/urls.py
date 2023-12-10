5f6db4b3a92eecb19abe39402f0e6b73a866d01d
iokninsk <01158211@pw.edu.pl>
2023-12-10 20:50:29 +0100

    Add endpoints prototypes

--- bowarto/backend/api/urls.py
+++ bowarto/backend/api/urls.py
@@ -4,7 +4,30 @@ from .views.competition import CompetitionList, CompetitionDetail
 from .views.index import index
 
 urlpatterns = [
-    path("", index, name="index"),
+    path('', index, name="index"),
+
+    # path('applications', ),
+    # path('applications/<int:id>', ),
+
     path('competitions', CompetitionList.as_view(), name='competition-list'),
-    path('competitions/<int:id>/', CompetitionDetail.as_view(), name='competition-detail')
+    path('competitions/<int:id>/', CompetitionDetail.as_view(), name='competition-detail'),
+
+    # path('files'),
+    # path('files/<int:id'),
+
+    # path('groups'),
+    # path('groups/<int:id>'),
+
+    # path('participants'),
+    # path('participants/<int:id>'),
+
+    # path('permissions'),
+    # path('permissions/<int:id>'),
+
+    # path('schools'),
+    # path('schools/<int:id>'),
+
+    # path('users'),
+    # path('users/<int:id>'),
+
 ]
