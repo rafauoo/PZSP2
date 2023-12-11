8df27230ff8e50807552f74ce325aed04e5cdd2a
rafauoo <rafal5094@gmail.com>
2023-12-11 14:51:52 +0100

    storage env

--- bowarto/backend/backend/custom_azure.py
+++ bowarto/backend/backend/custom_azure.py
@@ -1,13 +1,17 @@
 from storages.backends.azure_storage import AzureStorage
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'pzsp2storage' # Must be replaced by your <storage_account_name>
-    account_key = 'Us9XiJ4uxdcCxWMCCCE3v+ADhmO7X4waefYq9/7C9lI0lGQMh3yy3PSwPBWF6pgsThdvZkpR+ine+AStE6leSA==' # Must be replaced by your <storage_account_key>
+    account_name = env('STORAGE_NAME') # Must be replaced by your <storage_account_name>
+    account_key = env('STORAGE_KEY') # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'pzsp2storage' # Must be replaced by your storage_account_name
-    account_key = 'Us9XiJ4uxdcCxWMCCCE3v+ADhmO7X4waefYq9/7C9lI0lGQMh3yy3PSwPBWF6pgsThdvZkpR+ine+AStE6leSA==' # Must be replaced by your <storage_account_key>
+    account_name = env('STORAGE_NAME') # Must be replaced by your storage_account_name
+    account_key = env('STORAGE_KEY') # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
