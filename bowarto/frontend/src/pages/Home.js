58ecd2300c2f9b44849170e3745c66a75083daa8
rafauoo <rafal5094@gmail.com>
2023-12-12 21:05:55 +0100

    testing ci

--- bowarto/frontend/src/pages/Home.js
+++ bowarto/frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1 className="mt-5">Welcome!</h1>
+      <h1 className="mt-5">SIEMA!</h1>
       <p className="lead">
         Pin a fixed-height footer to the bottom of the viewport in desktop
         browsers with this custom HTML and CSS. A fixed navbar has been added
