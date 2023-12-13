0e7bec59fe96737311fe22522af633e6c5240a23
Zuzanna <42408601+avangie@users.noreply.github.com>
2023-12-13 18:21:23 +0100

    typo

--- bowarto/frontend/src/pages/Home.js
+++ bowarto/frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1 className="mt-5">Witamy na platfromie konkursowej BoWarto!</h1>
+      <h1 className="mt-5">Witamy na platformie konkursowej BoWarto!</h1>
     </div>
   );
 }
