6452841f1181f773d56b87ff4685d2aaa3f99318
iokninsk <01158211@pw.edu.pl>
2023-12-18 20:53:51 +0100

    Add filtering Participants by competition id.

--- bowarto/backend/api/views/participant.py
+++ bowarto/backend/api/views/participant.py
@@ -9,6 +9,14 @@ class ParticipantList(generics.ListCreateAPIView):
     queryset = Participant.objects.all()
     serializer_class = ParticipantSerializer
 
+    def get_queryset(self):
+        application_id = self.request.query_params.get('application')
+        if application_id is None:
+            return Participant.objects.all()
+        else:
+            return Participant.objects.filter(application_id=application_id)
+
+
 class ParticipantDetail(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     queryset = Participant.objects.all()
