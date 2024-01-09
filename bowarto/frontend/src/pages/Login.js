527c004f098ce1b964d27fe8e22a3d3ce23246d4
AndFirst <109021567+AndFirst@users.noreply.github.com>
2024-01-09 19:33:06 +0100

    Revert "Develop front user panel"

--- bowarto/frontend/src/pages/Login.js
+++ bowarto/frontend/src/pages/Login.js
@@ -1,8 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
-import {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [username, setUsername] = useState()
@@ -20,14 +20,13 @@ function Login() {
       email: username,
       password: password,
     };
-    axios.post("http://20.108.53.69/api/login/", postData, {})
+    axios.post("http://20.108.53.69/api/login/", postData, {
+    })
       .then(response => {
         setLoginError(false);
         const responseData = response.data;
-        const accessToken = responseData.access;
-        const refreshToken = responseData.refresh;
-        sessionStorage.setItem('access', accessToken);
-        sessionStorage.setItem('refresh', refreshToken);
+        const token = responseData.access;
+        sessionStorage.setItem('access', token);
       })
       .catch(error => {
         console.log('Login failed:', error.message);
@@ -35,9 +34,7 @@ function Login() {
         setLoginErrorMessage(JSON.stringify(error.response.data, null, 2));
       });
   };
-  const handleBack = () => {
-    navigate("/");
-  };
+  const handleBack = () => { navigate("/"); };
 
   const buttonStyle = {
     backgroundColor: 'rgb(131, 203, 83)',
@@ -58,25 +55,23 @@ function Login() {
         <Form onSubmit={handleLogin}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Nazwa użytkownika</Form.Label>
-            <Form.Control type="text" style={{width: '300px'}} placeholder="Podaj nazwę użytkownika"
-                          value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <Form.Control type="text" style={{ width: '300px' }} placeholder="Podaj nazwę użytkownika" value={username} onChange={(e) => setUsername(e.target.value)} />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Hasło</Form.Label>
-            <Form.Control type="password" placeholder="Podaj hasło" value={password}
-                          onChange={(e) => setPassword(e.target.value)}/>
+            <Form.Control type="password" placeholder="Podaj hasło" value={password} onChange={(e) => setPassword(e.target.value)} />
           </Form.Group>
           <div className="d-flex justify-content-center">
 
             {/* NOTE: this console.log("ERROR") should be changed */}
-            <button style={buttonStyle} variant="success" type="submit">
+            <button style={buttonStyle} variant="success" type="submit" >
               {/* <Button variant="success" type="submit" onClick={loginError ? console.log("ERROR") : () => handleBack()}> */}
               Zaloguj
             </button>
           </div>
         </Form>
-      </div>
+      </div >
     </>
   );
 }
--- bowarto/frontend/src/utils/format.js
+++
@@ -1,6 +0,0 @@
-function formatDate(dateString) {
-  const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'};
-  return new Date(dateString).toLocaleDateString('en-GB', options);
-}
-
-export default formatDate;
