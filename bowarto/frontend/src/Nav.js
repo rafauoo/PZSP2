744c6dd19a66be3d50fcd0a4bb4093a4c81ac87c
DawidBudzynsky <dawidbudzynski18@gmail.com>
2024-01-10 16:57:30 +0100

    changed refreshing navbar after login

--- bowarto/frontend/src/Nav.js
+++ bowarto/frontend/src/Nav.js
@@ -3,24 +3,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function NavbarExample() {
-  // WARNING: Now displayed link change after a page reload, not when the role is changed, idk why
-  const [role, setRole] = useState(sessionStorage.getItem('role'));
-
-  // Update the role state when the session storage changes
-  useEffect(() => {
-    setRole(sessionStorage.getItem('role'));
-  }, []);
+  const [role, setRole] = useState(sessionStorage.getItem('role'))
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="/">
-            <Image src="./images/logo.png" alt="Description" width={200}/>
+            <Image src="./images/logo.png" alt="Description" width={200} />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto justify-content-between">
               <Nav.Link href="/">Home</Nav.Link>
