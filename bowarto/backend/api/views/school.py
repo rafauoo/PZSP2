26f391c59080e2480bd1ce223b45e1d502e037a9
AndFirst <irekokninski12370@gmail.com>
2024-01-10 15:42:40 +0100

    Update school permissions

--- bowarto/backend/api/views/school.py
+++ bowarto/backend/api/views/school.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import authentication_classes
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from ..models import School
-from ..permissions import allow_admin, allow_admin_or_school_user
+from ..permissions import allow_admin, allow_admin_or_school_user, allow_authenticated, allow_any
 from ..serializers.school import SchoolSerializer
 
 
@@ -13,12 +13,13 @@ class SchoolList(generics.ListCreateAPIView):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
 
-    @allow_admin
+    @allow_any
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
-    @allow_admin
+    @allow_authenticated
     def post(self, request, *args, **kwargs):
+        # TODO Przy tworzeniu szkoły sprawdź czy istnieje, jeśli nie to utwórz
         return super().post(request, *args, **kwargs)
 
 
