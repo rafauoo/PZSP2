951a8a9719da5f6403e861f9dce49a5d8064777e
Zuzanna <42408601+avangie@users.noreply.github.com>
2024-01-11 11:45:34 +0100

    merge develop_front_dawid into master (#34)
    * changed refreshing navbar after login
    
    * primitive but working logout
    
    * cahnge the placement of a updateSession fucntion
    
    * createing register request
    
    * working registering a user
    
    * removed not used fields
    
    ---------
    
    Co-authored-by: DawidBudzynsky <dawidbudzynski18@gmail.com>

--- bowarto/frontend/src/Nav.js
+++ bowarto/frontend/src/Nav.js
@@ -3,30 +3,33 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import logout from './requests/logout';
 
 function NavbarExample() {
-  // WARNING: Now displayed link change after a page reload, not when the role is changed, idk why
-  const [role, setRole] = useState(sessionStorage.getItem('role'));
+  const [role, setRole] = useState(sessionStorage.getItem('role'))
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  // Update the role state when the session storage changes
   useEffect(() => {
-    setRole(sessionStorage.getItem('role'));
+    // Check if 'access' item is in sessionStorage
+    const token = sessionStorage.getItem('access');
+
+    // Update isLoggedIn based on the presence of 'access' item
+    setIsLoggedIn(!!token);
   }, []);
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="/">
-            <Image src="./images/logo.png" alt="Description" width={200}/>
+            <Image src="./images/logo.png" alt="Description" width={200} />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto justify-content-between">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/konkursy">Konkursy</Nav.Link>
               <Nav.Link href="/register">Rejestracja</Nav.Link>
-              <Nav.Link href="/login">Logowanie</Nav.Link>
               {role === 'admin' ? (
                 <>
                   <Nav.Link href="/createCompetition">Stwórz konkurs</Nav.Link>
@@ -38,6 +41,13 @@ function NavbarExample() {
                   <Nav.Link href="/user_panel">Moje aplikacje</Nav.Link>
                 </>
               ) : null}
+
+              {isLoggedIn ? (
+                <Nav.Link onClick={logout}>Wyloguj</Nav.Link>
+              ) : (
+                <Nav.Link href="/login">Logowanie</Nav.Link>
+              )}
+
             </Nav>
           </Navbar.Collapse>
         </Container>
