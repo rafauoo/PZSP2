f62890f18156072c94edcbb1e4fe59a1f00a6c71
AndFirst <irekokninski12370@gmail.com>
2024-01-13 17:21:20 +0100

    Debug file sending to participant

--- bowarto/backend/api/serializers/participant.py
+++ bowarto/backend/api/serializers/participant.py
@@ -1,35 +1,54 @@
 from rest_framework import serializers
-
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from .file import FileSerializer
 from ..models import Participant, File
 from ..utils import is_allowed_file_type
 
 
 class ParticipantSerializer(serializers.ModelSerializer):
-    attachment = serializers.PrimaryKeyRelatedField(
-        queryset=File.objects.all(),
-        required=False,
-        allow_null=True
-    )
+    attachment = FileSerializer(
+        required=False)  # Include FileSerializer for the 'attachment' field
 
     class Meta:
         model = Participant
-        fields = ['id', 'first_name', 'last_name', 'email', 'attachment', 'application']
+        fields = (
+            'id', 'email', 'application', 'first_name', 'last_name',
+            'attachment')
+
+    def create(self, validated_data):
+        # Extract 'attachment' data from validated_data if present
+        attachment_data = validated_data.pop('attachment', None)
+
+        # Create the participant instance
+        participant = Participant.objects.create(**validated_data)
+
+        # If 'attachment' data is present, create the File instance and associate it with the participant
+        if attachment_data:
+            file_instance = File.objects.create(**attachment_data)
+            participant.attachment = file_instance
+            participant.save()
+
+        return participant
 
     def update(self, instance, validated_data):
+        # Extract 'attachment' data from validated_data if present
         attachment_data = validated_data.pop('attachment', None)
-        # Perform the regular update on the participant instance
-        instance = super().update(instance, validated_data)
 
-        # Handle the update for the nested File field
+        # Update the participant instance
+        instance.email = validated_data.get('email', instance.email)
+        instance.application = validated_data.get('application',
+                                                  instance.application)
+        instance.first_name = validated_data.get('first_name',
+                                                 instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+
         if attachment_data:
-            # Check if the file type is allowed before saving
-            file_content = attachment_data.get('file').read()
-            if not is_allowed_file_type(file_content):
-                raise serializers.ValidationError("Invalid file type.")
+            if instance.attachment:
+                instance.attachment.delete()  # Delete the previous attachment
+            file_instance = instance.attachment if instance.attachment else File()
+            file_instance.path = attachment_data.get('path', file_instance.path)
+            file_instance.save()
+            instance.attachment = file_instance
 
-            attachment_serializer = FileSerializer(instance.attachment, data=attachment_data)
-            if attachment_serializer.is_valid():
-                attachment_serializer.save()
-
+        instance.save()
         return instance
--- bowarto/backend/api/views/participant.py
+++ bowarto/backend/api/views/participant.py
@@ -5,8 +5,11 @@ from rest_framework.decorators import authentication_classes
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from ..models import Participant, Application
-from ..permissions import allow_authenticated, allow_admin_or_participant_creator, allow_any, allow_admin
+from ..permissions import allow_authenticated, \
+    allow_admin_or_participant_creator, allow_any, allow_admin
+from ..serializers.file import FileSerializer
 from ..serializers.participant import ParticipantSerializer
+from django.shortcuts import get_object_or_404
 
 
 @authentication_classes([JWTAuthentication])
@@ -24,15 +27,19 @@ class ParticipantList(generics.ListCreateAPIView):
             return super().get(request, *args, **kwargs)
         if request.user.is_user:
             return self._get_participants_created_by_user(request.user)
-        return Response({'message': 'Not permitted'}, status=status.HTTP_403_FORBIDDEN)
+        return Response({'message': 'Not permitted'},
+                        status=status.HTTP_403_FORBIDDEN)
 
     def _get_participants_created_by_user(self, user):
         if self.request.query_params.get('application'):
             user_application = self.request.query_params.get('application')
-            user_participants = Participant.objects.filter(application=user_application, application__user=user)
+            user_participants = Participant.objects.filter(
+                application=user_application, application__user=user)
         else:
-            user_participants = Participant.objects.filter(application__user=user)
-        participant_serializer = ParticipantSerializer(user_participants, many=True)
+            user_participants = Participant.objects.filter(
+                application__user=user)
+        participant_serializer = ParticipantSerializer(user_participants,
+                                                       many=True)
         serialised_data = participant_serializer.data
         return Response(serialised_data, status=status.HTTP_200_OK)
 
@@ -42,16 +49,19 @@ class ParticipantList(generics.ListCreateAPIView):
             return super().post(request, *args, **kwargs)
         if request.user.is_user:
             return self._create_participant(request, *args, **kwargs)
-        return Response({'message': 'Not permitted'}, status=status.HTTP_403_FORBIDDEN)
+        return Response({'message': 'Not permitted'},
+                        status=status.HTTP_403_FORBIDDEN)
 
     def _create_participant(self, request, *args, **kwargs):
         application_id = request.data.get('application')
-        attachment_file = request.data.get('attachment')  # Assuming 'attachment' is the key for the file field
+        attachment_file = request.data.get(
+            'attachment')  # Assuming 'attachment' is the key for the file field
 
         try:
             application = Application.objects.get(id=application_id)
         except Application.DoesNotExist:
-            return Response({'message': 'Application not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'message': 'Application not found'},
+                            status=status.HTTP_404_NOT_FOUND)
 
         if application.user == request.user:
             # Create a dictionary with your data including the file
@@ -62,15 +72,19 @@ class ParticipantList(generics.ListCreateAPIView):
                 'last_name': request.data.get('last_name'),
                 'attachment': attachment_file,
             }
-            participant_serializer = ParticipantSerializer(data=participant_data)
+            participant_serializer = ParticipantSerializer(
+                data=participant_data)
 
             if participant_serializer.is_valid():
                 participant_serializer.save()
-                return Response(participant_serializer.data, status=status.HTTP_201_CREATED)
+                return Response(participant_serializer.data,
+                                status=status.HTTP_201_CREATED)
             else:
-                return Response(participant_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(participant_serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({'message': 'Not permitted'}, status=status.HTTP_403_FORBIDDEN)
+        return Response({'message': 'Not permitted'},
+                        status=status.HTTP_403_FORBIDDEN)
 
 
 @authentication_classes([JWTAuthentication])
@@ -83,13 +97,109 @@ class ParticipantDetail(generics.RetrieveUpdateDestroyAPIView):
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
-    @allow_admin_or_participant_creator
+    @allow_admin
     def put(self, request, *args, **kwargs):
-        return super().put(request, *args, **kwargs)
+        raise NotImplemented
 
-    @allow_admin_or_participant_creator
+    # def put(self, request, *args, **kwargs):
+    #     participant_id = kwargs.get('id')
+    #     try:
+    #         participant = Participant.objects.get(id=participant_id)
+    #     except Participant.DoesNotExist:
+    #         return Response({'error': 'Participant not found'},
+    #                         status=status.HTTP_404_NOT_FOUND)
+    #
+    #     # Check if 'attachment' key is present in the request data
+    #     if 'attachment' in request.data:
+    #         attachment_data = request.data['attachment']
+    #
+    #         # If 'attachment' data is 'null', delete the existing attachment
+    #         if attachment_data is None:
+    #             self._delete_attachment(participant)
+    #         else:
+    #             # Deserialize the participant data with or without the 'attachment' field
+    #             serializer = ParticipantSerializer(instance=participant,
+    #                                                data=request.data,
+    #                                                partial=True)
+    #
+    #             if serializer.is_valid():
+    #                 # Create a File instance from the file data
+    #                 file_serializer = FileSerializer(data=attachment_data)
+    #                 if file_serializer.is_valid():
+    #                     file_instance = file_serializer.save()
+    #
+    #                     # Set the file instance to the participant's attachment field
+    #                     participant.attachment = file_instance
+    #                 else:
+    #                     return Response({'error': 'Invalid file data'},
+    #                                     status=status.HTTP_400_BAD_REQUEST)
+    #
+    #             else:
+    #                 return Response(serializer.errors,
+    #                                 status=status.HTTP_400_BAD_REQUEST)
+    #
+    #     else:
+    #         # If 'attachment' key is not present, deserialize the participant data without the 'attachment' field
+    #         serializer = ParticipantSerializer(instance=participant,
+    #                                            data=request.data)
+    #
+    #         if not serializer.is_valid():
+    #             return Response(serializer.errors,
+    #                             status=status.HTTP_400_BAD_REQUEST)
+    #
+    #     # Save the participant instance
+    #     serializer.save()
+    #
+    #     return Response(serializer.data)
+    # @allow_admin_or_participant_creator
     def patch(self, request, *args, **kwargs):
-        return super().patch(request, *args, **kwargs)
+        participant_id = kwargs.get('id')
+        participant = get_object_or_404(Participant, id=participant_id)
+
+        # Check if 'attachment' key is present in the request data
+        if 'attachment' in request.data:
+            attachment_data = request.data['attachment']
+
+            # If attachment_data is 'null', delete the existing attachment
+            if attachment_data is None:
+                self._delete_attachment(participant)
+            else:
+                # Deserialize the participant data
+                serializer = ParticipantSerializer(participant,
+                                                   data=request.data,
+                                                   partial=True)
+
+                if serializer.is_valid():
+                    # Create a File instance from the file data
+                    file_serializer = FileSerializer(data=attachment_data)
+                    if file_serializer.is_valid():
+                        file_instance = file_serializer.save()
+
+                        # Set the file instance to the participant's attachment field
+                        participant.attachment = file_instance
+                    else:
+                        return Response({'error': 'Invalid file data'},
+                                        status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response(serializer.errors,
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+        # Save the participant instance
+        participant.save()
+
+        return Response({'message': 'Participant updated successfully'},
+                        status=status.HTTP_200_OK)
+
+    def _delete_attachment(self, participant):
+        if participant.attachment:
+            # Delete the existing attachment file
+            participant.attachment.path.delete()
+
+            # Delete the File instance
+            participant.attachment.delete()
+
+            # Set participant.attachment to null
+            participant.attachment = None
 
     @allow_admin_or_participant_creator
     def delete(self, request, *args, **kwargs):
